fbfe0dea3304c3bbe1dc9a5cdf6d1b98364fd211
Munaz Rahman <munaz_rahman@hotmail.com>
2016-03-19 19:01:07 -0400

    Initial setup, added date and time

---
+++ js/clock.js
@@ -0,0 +1,34 @@
+var tday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+var tmonth = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+$(function() {
+    GetClock();
+    setInterval(GetClock, 1000);
+});
+
+function GetClock() {
+    var d = new Date();
+    var nday = d.getDay(),
+        nmonth = d.getMonth(),
+        ndate = d.getDate(),
+        nyear = d.getYear();
+    if (nyear < 1000) nyear += 1900;
+    var nhour = d.getHours(),
+        nmin = d.getMinutes(),
+        nsec = d.getSeconds(),
+        ap;
+
+    if (nhour == 0) {
+        ap = " AM";
+        nhour = 12;
+    } else if (nhour < 12) { ap = " AM"; } else if (nhour == 12) { ap = " PM"; } else if (nhour > 12) {
+        ap = " PM";
+        nhour -= 12;
+    }
+
+    if (nmin <= 9) nmin = "0" + nmin;
+    if (nsec <= 9) nsec = "0" + nsec;
+
+    $("#date").html(tday[nday] + ", " + tmonth[nmonth] + " " + ndate + ", " + nyear);
+    $("#time").html(nhour + ":" + nmin + ":" + nsec + ap);
+}
